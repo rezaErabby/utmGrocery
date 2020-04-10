2a50aba88bfb2dc2e00dc93ccbdbe8cd7aa64191
bcgeworld <leaderanonymous711@gmail.com>
2020-04-10 17:51:27 +0800

    everything added

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:utmgrocery/Pages/profile.dart';
+import 'package:utmgrocery/Pages/register.dart';
+import 'package:utmgrocery/Pages/splash.dart';
+import 'package:utmgrocery/Pages/login.dart';
+
+void main() => runApp(MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Splash(),
+        '/login': (context) => Login(),
+        '/register': (context) => Register(),
+        '/profile': (context) => Profile(),
+      },
+    ));
