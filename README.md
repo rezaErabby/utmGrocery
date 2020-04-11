89cbeaf2ab37f4e231d6496cb93a545671278bd9
bcgeworld <leaderanonymous711@gmail.com>
2020-04-11 23:36:47 +0800

    notification page done

--- README.md
+++ README.md
@@ -14,3 +14,10 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter, view our
 [online documentation](https://flutter.dev/docs), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+# Project Name : UTMGrocery
+# Members : 
+# Reza-E-Rabby A17CS4032
+# Syed Muhammad Yamin Gharbi A17CS5066
+# Akibul Hasan A17CS4015
+# Nazmus Sakib A17CS0265
--- lib/Pages/notification.dart
+++ lib/Pages/notification.dart
@@ -4,10 +4,60 @@ class Notifications extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Notifications"),
-        centerTitle: true,
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Notification'),
+          centerTitle: true,
+          backgroundColor: Color.fromRGBO(122, 207, 122, 1),
+        ),
+        body: SafeArea(
+          child: Center(
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.start, children: <
+                    Widget>[
+              Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.message,
+                      color: Colors.teal[900],
+                    ),
+                    title: Text(
+                      'Reza',
+                      style: TextStyle(fontFamily: 'BalooBhai', fontSize: 20.0),
+                    ),
+                  )),
+              Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.message,
+                      color: Colors.teal[900],
+                    ),
+                    title: Text(
+                      'Sakib',
+                      style: TextStyle(fontFamily: 'BalooBhai', fontSize: 20.0),
+                    ),
+                  )),
+              Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.message,
+                      color: Colors.teal[900],
+                    ),
+                    title: Text(
+                      'Akib',
+                      style: TextStyle(fontFamily: 'BalooBhai', fontSize: 20.0),
+                    ),
+                  )),
+            ]),
+          ),
+        ));
   }
 }
