4b2f4b85fb2e854f7b3ca830f12ea2d41b71ac98
bcgeworld <leaderanonymous711@gmail.com>
2020-04-11 22:53:09 +0800

    profile page updated

--- lib/Pages/profile.dart
+++ lib/Pages/profile.dart
@@ -46,19 +46,22 @@ class Profile extends StatelessWidget {
                   child: Container(
                     margin: EdgeInsets.only(top: 170),
                     child: Center(
-                      child: Text(
-                        "Mark Zukerburg",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 30,
-                            fontWeight: FontWeight.bold),
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 23.0),
+                        child: Text(
+                          "Mark Zukerburg",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold),
+                        ),
                       ),
                     ),
                   ),
                 ),
                 Positioned(
                     top: 250,
-                    left: 130,
+                    left: 135,
                     child: Container(
                         child: Center(
                             child: FlatButton.icon(
@@ -71,6 +74,132 @@ class Profile extends StatelessWidget {
                     )))),
               ]),
             ),
+            SafeArea(
+                child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Card(
+                      elevation: 1.0,
+                      margin: const EdgeInsets.fromLTRB(8.0, 5.0, 8.0, 8.0),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0)),
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.person,
+                          color: Colors.black,
+                        ),
+                        title: Text(
+                          'Name',
+                        ),
+                      )),
+                  Card(
+                    elevation: 1.0,
+                    margin: const EdgeInsets.fromLTRB(8.0, 5.0, 8.0, 8.0),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0)),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.email,
+                        color: Colors.black,
+                      ),
+                      title: Text(
+                        'Email',
+                      ),
+                    ),
+                  ),
+                  Card(
+                    elevation: 1.0,
+                    margin: const EdgeInsets.fromLTRB(8.0, 5.0, 8.0, 8.0),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0)),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.supervised_user_circle,
+                        color: Colors.black,
+                      ),
+                      title: Text(
+                        'Username',
+                      ),
+                    ),
+                  ),
+                  Card(
+                    elevation: 1.0,
+                    margin: const EdgeInsets.fromLTRB(8.0, 5.0, 8.0, 8.0),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0)),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.home,
+                        color: Colors.black,
+                      ),
+                      title: Text(
+                        'Address',
+                      ),
+                    ),
+                  ),
+                  Card(
+                    elevation: 1.0,
+                    margin: const EdgeInsets.fromLTRB(8.0, 5.0, 8.0, 8.0),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0)),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.phone,
+                        color: Colors.black,
+                      ),
+                      title: Text(
+                        'Phone Number',
+                      ),
+                    ),
+                  ),
+                  Card(
+                    elevation: 1.0,
+                    margin: const EdgeInsets.fromLTRB(8.0, 5.0, 8.0, 8.0),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0)),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.shopping_cart,
+                        color: Colors.black,
+                      ),
+                      title: Text(
+                        'My Orders',
+                      ),
+                    ),
+                  ),
+                  Card(
+                    elevation: 1.0,
+                    margin: const EdgeInsets.fromLTRB(8.0, 5.0, 8.0, 8.0),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0)),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.shopping_basket,
+                        color: Colors.black,
+                      ),
+                      title: Text(
+                        'My Delivery',
+                      ),
+                    ),
+                  ),
+                  Card(
+                    elevation: 1.0,
+                    margin: const EdgeInsets.fromLTRB(8.0, 5.0, 8.0, 8.0),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0)),
+                    child: ListTile(
+                      leading: Icon(Icons.location_on, color: Colors.black),
+                      title: Text("My Location"),
+                      trailing: Switch(
+                          activeColor: Colors.green,
+                          value: true,
+                          onChanged: (val) {}),
+                    ),
+                  ),
+                ],
+              ),
+            )),
           ]),
         ),
       ),
