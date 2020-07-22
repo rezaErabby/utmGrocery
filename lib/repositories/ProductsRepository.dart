b18b096e788d60195a00198b9cd9008acac2b141
rezaerabby <rezaerabby015@gmail.com>
2020-07-22 13:23:46 +0800

    componenets and views files are added

--- lib/repositories/ProductsRepository.dart
+++ lib/repositories/ProductsRepository.dart
@@ -3,12 +3,12 @@ import 'package:utmgrocery/models/Product.dart';
 class ProductsRepository {
   List<Product> fetchAllProducts() {
     return [
-      new Product("assets/images/mango.png", "Organic Mangoes", 5.0, 1.0, 0),
-      new Product("assets/images/watermelon.png", "Watermelon", 4.0, 1.0, 1),
-      new Product("assets/images/apple.png", "Red Apples", 3.5, 1.0, 2),
+      new Product("assets/images/mango.png", "Organic Mangoes", 5.0, 750, 0),
+      new Product("assets/images/watermelon.png", "Watermelon", 4.0, 650, 1),
+      new Product("assets/images/apple.png", "Red Apples", 3.5, 500, 2),
       new Product("assets/images/honey.jpg", "Natural Honey", 25.00, 250, 3),
       new Product("assets/images/vitamins.png", "Multi-Vitamins", 39.0, 250, 4),
-      new Product("assets/images/orange.png", "Organic Oranges", 3.5, 1.0, 5),
+      new Product("assets/images/orange.png", "Organic Oranges", 3.5, 500, 5),
       new Product("assets/images/cum.png", "Cucumber", 1.99, 500, 6),
     ];
   }
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,61 +1,110 @@
 import 'package:flutter/material.dart';
+import 'package:utmgrocery/bloc/CartBloc.dart';
+import 'package:utmgrocery/components/CartManager.dart';
+import 'package:utmgrocery/components/GridShop.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool _showCart = false;
+  CartBloc _cartBloc;
+
+  ScrollController _scrollController = new ScrollController();
+
+  @override
+  initState() {
+    _scrollController = new ScrollController();
+    _cartBloc = new CartBloc();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: NavDrawer(),
-      appBar: AppBar(
-        title: Text('UTM Grocery'),
-        centerTitle: true,
         backgroundColor: Colors.green[200],
-      ),
-      body: Center(
-        child: Text("Home page"),
-      ),
-    );
-  }
-}
-
-class NavDrawer extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: <Widget>[
-          SafeArea(
-            child: DrawerHeader(
-              child: Text(
-                "UTM Grocery",
-                style: TextStyle(color: Colors.white, fontSize: 25),
-              ),
-              decoration: BoxDecoration(
-                color: Colors.green,
-                image: DecorationImage(
-                  fit: BoxFit.fill,
-                  image: AssetImage('assets/images/logo.png'),
+        appBar: AppBar(
+          title: Text('UTM Grocery'),
+          centerTitle: true,
+          backgroundColor: Colors.green[200],
+        ),
+        drawer: new Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              SafeArea(
+                child: DrawerHeader(
+                  child: Text(
+                    "UTM Grocery",
+                    style: TextStyle(color: Colors.white, fontSize: 25),
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    image: DecorationImage(
+                      fit: BoxFit.fill,
+                      image: AssetImage('assets/images/logo.png'),
+                    ),
+                  ),
                 ),
               ),
-            ),
+              ListTile(
+                leading: Icon(Icons.verified_user),
+                title: Text("Profile"),
+                onTap: () => Navigator.pushNamed(context, '/profile'),
+              ),
+              ListTile(
+                leading: Icon(Icons.settings),
+                title: Text('Settings'),
+                onTap: () => Navigator.pushNamed(context, '/settings'),
+              ),
+              ListTile(
+                leading: Icon(Icons.exit_to_app),
+                title: Text('Logout'),
+                onTap: () => Navigator.pushNamed(context, '/login'),
+              ),
+            ],
           ),
-          ListTile(
-            leading: Icon(Icons.verified_user),
-            title: Text("Profile"),
-            onTap: () => Navigator.pushNamed(context, '/profile'),
-          ),
-          ListTile(
-            leading: Icon(Icons.settings),
-            title: Text('Settings'),
-            onTap: () => Navigator.pushNamed(context, '/settings'),
-          ),
-          ListTile(
-            leading: Icon(Icons.exit_to_app),
-            title: Text('Logout'),
-            onTap: () => Navigator.pushNamed(context, '/login'),
-          ),
-        ],
-      ),
-    );
+        ),
+        body: new Stack(children: <Widget>[
+          new CustomScrollView(
+              physics: NeverScrollableScrollPhysics(),
+              controller: _scrollController,
+              slivers: <Widget>[
+                new SliverToBoxAdapter(child: new GridShop()),
+                new SliverToBoxAdapter(child: new CartManager()),
+              ]),
+          new Align(
+              alignment: Alignment.bottomRight,
+              child: new Container(
+                  margin: EdgeInsets.only(right: 10, bottom: 10),
+                  child: new FloatingActionButton(
+                      onPressed: () {
+                        if (_showCart)
+                          _scrollController.animateTo(
+                              _scrollController.position.minScrollExtent,
+                              curve: Curves.fastOutSlowIn,
+                              duration: Duration(seconds: 2));
+                        else
+                          _scrollController.animateTo(
+                              _scrollController.position.maxScrollExtent,
+                              curve: Curves.fastOutSlowIn,
+                              duration: Duration(seconds: 2));
+
+                        setState(() {
+                          _showCart = !_showCart;
+                        });
+                      },
+                      backgroundColor: Colors.green[300],
+                      child: new Icon(
+                          _showCart ? Icons.close : Icons.shopping_cart))))
+        ]));
+  }
+
+  @override
+  void dispose() {
+    _cartBloc.dispose();
+    super.dispose();
   }
 }
